27177639789a36b2309cadaaafb21aa47fee4f8a
soymou <emilio.junoy@gmail.com>
2026-01-09 19:23:26 -0600

    Manage configurations with home-manager and use proper icons

--- home-modules/environment.nix
+++ home-modules/environment.nix
@@ -1,21 +1,25 @@
-inputs:
-
-{ config, lib, pkgs, ... }:
-
-let
+inputs: {
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
   cfg = config.programs.illogical-impulse;
   pythonEnv = cfg.internal.pythonEnv;
-in
-{
+in {
   config = lib.mkIf cfg.enable {
     # Environment variables for Illogical Impulse
     home.sessionVariables = {
-      QT_QPA_PLATFORMTHEME = "qt6ct";  # Use qt6ct for Qt6 theming
+      # QT_QPA_PLATFORMTHEME = "qt6ct";  # Use qt6ct for Qt6 theming
       QT_STYLE_OVERRIDE = "";
       ILLOGICAL_IMPULSE_DOTFILES_SOURCE = "${config.home.homeDirectory}/.config";
+      qsConfig = "${config.home.homeDirectory}/.config/quickshell/ii";
     };
 
+    # Ensure variables are available to systemd services (and Hyprland)
+    systemd.user.sessionVariables = config.home.sessionVariables;
+
     # Install qt6ct for Qt theming
-    home.packages = [ pkgs.qt6Packages.qt6ct ];
+    home.packages = [pkgs.qt6Packages.qt6ct];
   };
 }
